c1beeb92b7923360af48168632c678fc6b9fed15
Ashok Kumar Kurva <63834327+ashok-kumar-21@users.noreply.github.com>
2024-02-11 21:56:34 +0530

    Update Middle of LinkedList.java

--- LinkedList/Middle of LinkedList.java
+++ LinkedList/Middle of LinkedList.java
@@ -19,3 +19,13 @@ class Solution {
         return slowptr;
     }
 }
+/**
+Approach: 
+1. We take slow pointer and fast pointer and increase fast pointer by two times and slow pointer by one time. 
+2. Once fast pointer reaches the end of the LL, slow pointer travels half the distance and reaches the middle 
+of the linkedlist.
+3. Atlast, we return slowpointer.
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+

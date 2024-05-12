729c72cf072db7cc25a2286fb7a2a394a5224884
Ashok Kumar Kurva <63834327+ashok-kumar-21@users.noreply.github.com>
2024-05-12 21:39:27 +0530

    Kadane's Algorithm (Max subarray Sum).java

--- Arrays/3. Kadane's Algorithm (Max subarray Sum).java
+++ Arrays/3. Kadane's Algorithm (Max subarray Sum).java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSubArraySum = Integer.MIN_VALUE;
+        int maxSoFar = 0;
+        int start = 0;
+        int end = nums.length-1;
+        for(int i=0; i<nums.length; i++){
+            maxSoFar += nums[i];
+            if(maxSoFar > maxSubArraySum){
+                maxSubArraySum = maxSoFar;
+                end = i;
+            }
+            if(maxSoFar<0){
+                maxSoFar = 0;
+                start = i+1;
+            }
+        }
+        System.out.println(start);
+        System.out.println(end);
+        return maxSubArraySum;
+    }
+}
